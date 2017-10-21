6630b08cb6801454015306e5c3823e9a834448bd
Vilius Drumsta <vdrumsta@gmail.com>
2017-10-21 15:08:07 +0100

    Speech bubbles display 1 icon

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class SpeechBubble : MonoBehaviour {
 
+    public GameObject images;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void DisplaySpeechBubble() {
+        images.SetActive(true);
+        Invoke("DisableSpeechBubble", 2);
+    }
+
+    private void DisableSpeechBubble() {
+        images.SetActive(false);
+    }
 }
