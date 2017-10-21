19c453487092085f2a9e887da071d2eeea130d39
James Daly <14152789@studentmail.ul.ie>
2017-10-21 16:08:16 +0100

    Added cameras

--- Assets/Scripts/Movement/PlayerController.cs
+++ Assets/Scripts/Movement/PlayerController.cs
@@ -6,18 +6,26 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float velocity;
+    float moveHorizontal;
+    float moveVertical;
 
     private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        moveHorizontal = 0.0f;
+        moveVertical = 0.0f;
+    }
+
+    private void Update()
+    {
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxisRaw ("Horizontal");
-        float moveVertical = Input.GetAxisRaw ("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.AddForce (movement * speed);
         /*if (rb2d.velocity == velocityCheck)
