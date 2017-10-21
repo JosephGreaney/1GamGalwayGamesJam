6600754cdee061268f91aeb3e96e38b0b6c063ac
NemoEtNihil <jameson.stephen97@gmail.com>
2017-10-21 16:12:18 +0100

    Character
    Character prefab, scripts

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+	public class Character
+	{
+        private static List<string> names = new List<string> { "Larry", "Bob", "Lucy" };
+        private static List<string> weaknesses = new List<string> { "Anime", "Beer", "Food" };
+        private string name;
+		private string weakness;
+
+        public Character()
+		{
+            name = names[Random.Range(0, names.Count - 1)];
+            weakness = weaknesses[Random.Range(0, weaknesses.Count - 1)];
+            names.Remove(name);
+            weaknesses.Remove(weakness);
+        }
+
+		public string GetName()
+		{
+			return name;
+		}
+
+		public string GetWeakness()
+		{
+			return weakness;
+		}
+	}
+}
