357ea23228b60757ce8c952ee2d0c778294cd072
Matthew Murphy <hellomatthewmurphy@gmail.com>
2017-10-21 18:59:06 +0100

    Interaction mostly done

---
+++ Assets/Scripts/Interaction/PersonInteraction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonInteraction : MonoBehaviour, Interaction
+{
+    public bool highlighted;
+    public string word;
+
+    // Use this for initialization
+    void Start()
+    {
+        highlighted = false;
+        word = "Hey there friend";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }  
+
+    public bool GetHighlighted()
+    {
+        return highlighted;
+    }
+
+    public void SetHighlighted(bool highlighted)
+    {
+        this.highlighted = highlighted;
+    }
+
+    public void DoActionNow(GameObject caller)
+    {
+        if (highlighted)
+            Debug.Log(word);
+    }
+}
