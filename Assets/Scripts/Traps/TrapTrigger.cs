ea95355270c55e01f0adb7e53367e635228a74d0
JosephGreaney <josephgreaney@gmail.com>
2017-10-22 12:15:09 +0100

    Add wardrobe falling animation and logic.

---
+++ Assets/Scripts/Traps/TrapTrigger.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+[RequireComponent (typeof(Animator))]
+public class TrapTrigger : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<Character>() != null)
+        {
+            anim.SetTrigger("Activate");
+        }
+    }
+}
