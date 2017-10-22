06db804a0b75fa5da1dd8b4d0250113b418b6502
JosephGreaney <josephgreaney@gmail.com>
2017-10-22 10:36:51 +0100

    Add car trap which kills a character.

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent (typeof (Animator))]
+public class Character : MonoBehaviour
+{
+    private Animator anim;
+    public bool IsDead { get; set; }
+    
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        IsDead = false;
+    }
+
+    public void Kill()
+    {
+        if (!IsDead)
+        {
+            IsDead = true;
+            anim.SetBool("IsDead", true);
+        }
+    }
+}
