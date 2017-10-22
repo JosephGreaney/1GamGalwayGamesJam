8fccd0e82bdd26e61f4b198f78fb5f80ba505897
James Daly <14152789@studentmail.ul.ie>
2017-10-22 14:48:05 +0100

    Working on event logos

---
+++ Assets/Scripts/FireEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireEvent : MonoBehaviour {
+
+    public GameObject textBox;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        textBox.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        textBox.SetActive(false);
+    }
+}
